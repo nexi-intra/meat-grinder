de509fa3e8f2c61c4a10458f82508a8a7bdb4bfa
Niels Gregers Johansen <niels@timeboxer.dk>
2024-12-12 11:37:43 +0100

    baseline

---
+++ 00-connectors/sharepoint/install.ps1
@@ -0,0 +1,3 @@
+write-host "Installing PnP PowerShell" -ForegroundColor Green
+Install-Module -Name PnP.PowerShell -Force -AllowPrerelease -Scope CurrentUser
+
---
+++ 10-environments/graph-postgres/check.ps1
@@ -0,0 +1,31 @@
+
+$requiredVars = @(
+  'DEVICEKPIBLOB',
+  'GRAPH_APPID',
+  'GRAPH_APPSECRET',
+  'GRAPH_APPDOMAIN',
+  'GRAPH_ACCESSTOKEN',
+  'POSTGRES_SERVER'
+  
+)
+
+# Enumerate all environment variables
+$allEnv = Get-ChildItem Env:
+
+# Store environment variables in a hashtable for easy lookup
+$envHash = @{}
+$allEnv | ForEach-Object {
+  $envHash[$_.Name] = $_.Value
+}
+  
+# Check which required variables are missing or empty
+$missingVars = $requiredVars | Where-Object { -not $envHash[$_] -or $envHash[$_] -eq "" }
+  
+if ($missingVars.Count -gt 0) {
+  Write-Host "The following required environment variables are missing or empty:"
+  $missingVars | ForEach-Object { Write-Host "- $_" }
+  exit 1
+}
+else {
+  Write-Host "All required environment variables are present."
+}
